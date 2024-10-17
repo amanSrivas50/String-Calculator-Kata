7c59959890531875a1b6cb9043a39790b2d8ea20
gus-ahr <amansrivas50@gmail.com>
2024-10-17 19:52:11 +0530

    custom delimiter

--- string-calculator.spec.ts
+++ string-calculator.spec.ts
@@ -4,36 +4,44 @@ import { StringCalculator } from "./string-calculator";
 let input: string;
 let result: number;
 
-describe('without delimiter', () => {
+test('empty string', () => {
+    input = '';
+    result = StringCalculator.add(input);
+    expect(result).toBe(0);
+})
 
-    it('empty string', () => {
-        input = '';
-        result = StringCalculator.add(input);
-        expect(result).toBe(0);
-    })
+test('single input', () => {
+    input = '1';
+    result = StringCalculator.add(input);
+    expect(result).toBe(1);
+})
 
-    it('single input', () => {
-        input = '1';
-        result = StringCalculator.add(input);
-        expect(result).toBe(1);
-    })
+test('multiple input', () => {
+    input = '1,5';
+    result = StringCalculator.add(input);
+    expect(result).toBe(6);
+})
 
-    it('multiple input', () => {
-        input = '1,5';
-        result = StringCalculator.add(input);
-        expect(result).toBe(6);
-    })
+test('input in multiple lines 1', () => {
+    input = '1\n2,3';
+    result = StringCalculator.add(input);
+    expect(result).toBe(6);
+})
 
-    it('input in multiple lines 1', () => {
-        input = '1\n2,3';
-        result = StringCalculator.add(input);
-        expect(result).toBe(6);
-    })
-
-    it('input in multiple lines 2', () => {
-        input = '1\n2,3\n2';
-        result = StringCalculator.add(input);
-        expect(result).toBe(8);
-    })
+test('input in multiple lines 2', () => {
+    input = '1\n2,3\n2';
+    result = StringCalculator.add(input);
+    expect(result).toBe(8);
+})
     
+test('custom delimiter 1', () => {
+    input = '//;\n1;2';
+    result = StringCalculator.add(input);
+    expect(result).toBe(3);
+})
+
+test('custom delimiter 2', () => {
+    input = '//*$\n1*$2*$4';
+    result = StringCalculator.add(input);
+    expect(result).toBe(7);
 })
--- string-calculator.ts
+++ string-calculator.ts
@@ -1,15 +1,31 @@
 export class StringCalculator {
     
-    static readonly regex = /,|\n/; // for comma and new line
+    static readonly REGEX = /,|\n/; // for comma and new line
+
+    static readonly startOfCustomDelimiter = '//';
+    
+    static readonly endOfCustomDelimiter ='\n';
 
     static add(numbers: string): number {
-        const inputNumberArray = numbers.split(StringCalculator.regex)
+        let delimiter:any = StringCalculator.REGEX;
+        if (numbers.startsWith(StringCalculator.startOfCustomDelimiter)) {
+            const indexOfEndOfCustomDelimiter = numbers.indexOf(StringCalculator.endOfCustomDelimiter);
+            delimiter = StringCalculator.getCustomDelimiter(numbers,indexOfEndOfCustomDelimiter);
+            numbers = numbers.slice(indexOfEndOfCustomDelimiter + 1);
+        }
+        const inputNumberArray = numbers.split(delimiter)
         let sum: number = 0;
         inputNumberArray.forEach((number) => {
             if (isNaN(Number(number))) throw Error("Invalid Output");
             sum += Number(number);
         })
         return sum;
+    }
+
+
+    static getCustomDelimiter(numbers: string,indexOfEndOfCustomDelimiter:number): string{
+        if(indexOfEndOfCustomDelimiter<0) throw Error("Invalid Output");
+        return numbers.slice(2, indexOfEndOfCustomDelimiter);
     }
 }
 
